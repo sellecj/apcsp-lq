1633010d3d305e4c7f17ac2503e84b3d28506b70
Collin Selle <sellecollin@gmail.com>
2019-10-08 09:22:05 -0600

    Completes quiz

--- list_quiz.rb
+++ list_quiz.rb
@@ -13,18 +13,18 @@ def three_even? (list)
         end
         list_position += 1
     end
-    if even_count == 3 && next_to == true
+    if even_count == 3 && next_to 
         return true
     else
         return false
     end
 end
 
-# puts three_even?([2, 1, 3, 5])
-# puts three_even?([2, 4, 12, 5])
-# puts three_even?([2, 1, 4, 6])
-# puts three_even?([1, 4, 6, 4])
-# puts three_even?([])
+puts three_even?([2, 1, 3, 5])
+puts three_even?([2, 4, 12, 5])
+puts three_even?([2, 1, 4, 6])
+puts three_even?([1, 4, 6, 4])
+puts three_even?([])
 
 def bigger_two (list1, list2)
     sum1 = list1[0] + list1[1]
@@ -36,15 +36,17 @@ def bigger_two (list1, list2)
     end
 end
 
-# print bigger_two([1, 2], [3, 4])
-# print bigger_two([1, 7], [4, 4])
+print bigger_two([1, 2], [3, 4]) 
+puts ""
+print bigger_two([1, 7], [4, 4])
+puts ""
 
 def series_up (n)
     list = []
     series = 1
-    n.times do |i|
-        series.times do |s|
-            list.push(s+1)
+    n.times do 
+        series.times do |i|
+            list.push(i+1)
         end
         series += 1
     end
@@ -52,6 +54,9 @@ def series_up (n)
 end
 
 print series_up(1) 
-print series_up(2)
-print series_up(3)
+puts ""
+print series_up(2) 
+puts ""
+print series_up(3) 
+puts ""
 print series_up(4)
